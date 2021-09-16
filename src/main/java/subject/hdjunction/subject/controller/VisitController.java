dbce633b8c69dbc892bb02ec5916b5f25ef0ed02
kiomnd2 <kiomnd2@naver.com>
2021-09-16 22:12:50 +0900

    Visit 기본 API 클래스 정의

---
+++ src/main/java/subject/hdjunction/subject/controller/VisitController.java
@@ -0,0 +1,41 @@
+package subject.hdjunction.subject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import subject.hdjunction.subject.codes.Codes;
+import subject.hdjunction.subject.controller.response.Response;
+import subject.hdjunction.subject.dto.PatientDto;
+import subject.hdjunction.subject.dto.VisitDto;
+import subject.hdjunction.subject.service.VisitService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class VisitController {
+
+    final private VisitService visitService;
+
+
+    @PostMapping("/api/v1/visit")
+    public ResponseEntity<Response<VisitDto>> register(@RequestBody VisitDto visitDto) {
+        VisitDto visit = visitService.registerVisit(visitDto);
+        return ResponseEntity.ok().body(Response.success(visit));
+    }
+
+    @GetMapping("/api/v1/visit/{id}")
+    public ResponseEntity<Response<VisitDto>> searchInfo(@PathVariable(name = "id") Long patientId) {
+        return ResponseEntity.ok().body(Response.success(visitService.getVisit(patientId)));
+    }
+
+    @PutMapping("/api/v1/visit/{id}")
+    public ResponseEntity<Response<VisitDto>> updateInfo(@PathVariable(name = "id") Long patientId, VisitDto visitDto) {
+        return ResponseEntity.ok().body(Response.success(visitService.updateVisit(patientId, visitDto)));
+    }
+
+    @DeleteMapping("/api/v1/visit/{id}")
+    public ResponseEntity<Response<String>> deleteInfo(@PathVariable(name = "id") Long patientId) {
+        return ResponseEntity.ok().body(Response.success(Codes.E4000.desc));
+    }
+}
