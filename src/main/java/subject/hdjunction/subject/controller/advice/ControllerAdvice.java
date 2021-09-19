c44f6abeb12dc2405674a89905f23eac54b9cb29
kiomnd2 <kiomnd2@naver.com>
2021-09-20 02:12:55 +0900

    익셉션 관리를 위한 controllerAdvice 수정

--- src/main/java/subject/hdjunction/subject/controller/advice/ControllerAdvice.java
+++ src/main/java/subject/hdjunction/subject/controller/advice/ControllerAdvice.java
@@ -6,22 +6,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import subject.hdjunction.subject.controller.response.Response;
-import subject.hdjunction.subject.exception.NotFoundHospitalException;
-import subject.hdjunction.subject.exception.NotFoundPatientException;
+import subject.hdjunction.subject.exception.*;
 
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler(NotFoundPatientException.class)
+    @ExceptionHandler({NotFoundPatientException.class, NotFoundHospitalException.class,
+            NotFoundVisitException.class, NotFoundCodeException.class, NotFoundCodeGroupException.class,
+            CodeInjectionException.class
+    })
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Response<String>> validate(NotFoundPatientException e) {
+    public ResponseEntity<Response<String>> validate(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Response.fail(e.getMessage()));
     }
 
-    @ExceptionHandler(NotFoundHospitalException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Response<String>> validate(NotFoundHospitalException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Response.fail(e.getMessage()));
-    }
+
 
 }
